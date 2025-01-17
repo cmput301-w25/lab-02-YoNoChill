fa42dcd1c3f962cf92eba8146e79203171a32cf1
Aari Vinayagasothy <aari.vinayagasothy@gmail.com>
2025-01-16 19:14:24 -0700

    Lab 2 Participation Complete!!

---
+++ ListyCityCode/app/src/main/java/com/example/listycity/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.listycity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    ListView cityList;
+    Button button;
+    TextView input;
+    ArrayAdapter<String> cityAdapter;
+    ArrayList<String> dataList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EdgeToEdge.enable(this);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        cityList = findViewById(R.id.city_list);
+        input = findViewById(R.id.Input);
+        button = findViewById(R.id.button3);
+
+
+        String []cities = {"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"};
+        dataList = new ArrayList<>();
+        dataList.addAll(Arrays.asList(cities));
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityList.setAdapter(cityAdapter);
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                dataList.remove(i);
+                cityAdapter.notifyDataSetChanged();
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = input.getText().toString();
+                dataList.add(text);
+                cityAdapter.notifyDataSetChanged();
+            }
+        });
+
+    }
+}
